8f9a09efef59bf9432594d897e1d142cac16e533
José Díaz <73610677+Pp-wveDev@users.noreply.github.com>
2020-12-13 01:28:33 +0100

    Añadida barra inferior
    Faltan alinear elementos bien

--- app/src/main/java/com/example/controlgastos/MainActivity.java
+++ app/src/main/java/com/example/controlgastos/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.controlgastos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
